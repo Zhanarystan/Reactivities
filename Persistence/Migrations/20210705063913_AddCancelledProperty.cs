48eb87291277e63b4bee0abe99681a4ed9870aee
Zhanarystan <mukhtarkhanov25@gmail.com>
2021-07-05 19:40:16 +0600

    End of section 14

---
+++ Persistence/Migrations/20210705063913_AddCancelledProperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddCancelledProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MyProperty",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Activities");
+        }
+    }
+}
