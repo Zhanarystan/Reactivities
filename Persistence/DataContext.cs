0cb9ba6220270d1286c48abea00cc1ea27e67124
Zhanarystan <mukhtarkhanov25@gmail.com>
2021-07-13 10:54:21 +0600

    End of section 19

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -15,6 +15,7 @@ namespace Persistence
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
 
         public DbSet<Photo> Photos {get;set;}
+        public DbSet<Comment> Comments {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -32,7 +33,10 @@ namespace Persistence
                 .WithMany(a => a.Attendees)
                 .HasForeignKey(aa => aa.ActivityId);
 
-            
+            builder.Entity<Comment>()
+                .HasOne(a => a.Activity)
+                .WithMany(c => c.Comments)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommentStore from "./commentStore";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
 import ProfileStore from "./profileStore";
@@ -8,9 +9,10 @@ import UserStore from "./userStore";
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
-    userStore: UserStore;
+    userStore: UserStore;  
     modalStore: ModalStore;
     profileStore: ProfileStore;
+    commentStore: CommentStore;
 }
 
 export const store: Store = {
@@ -18,7 +20,8 @@ export const store: Store = {
     commonStore: new CommonStore(),
     userStore: new UserStore(),
     modalStore: new ModalStore(),
-    profileStore: new ProfileStore()
+    profileStore: new ProfileStore(),
+    commentStore: new CommentStore()
 }
 
 export const StoreContext = createContext(store);
